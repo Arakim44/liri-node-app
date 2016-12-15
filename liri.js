ff2e03fd3da015bad6bdc69ca8d60049cd1fa96d
Ara Kim <ahrakim44@gmail.com>
2016-12-15 14:28:41 -0600

    beautify

--- liri.js
+++ liri.js
@@ -49,22 +49,22 @@ switch (action) {
 
 
             //This if statement is when user put undefined track title.
-            if (data.tracks.items[0] === undefined){
-              console.log("\n-------------\n");
-              console.log(" We could not fined what you searched for.\n")
-              console.log(" Song Title: The Sign\n Artist Name: Ace of Base\n Album Name: The Sign(US Album)\n Preview Link: https://p.scdn.co/mp3-preview/177e65fc2b8babeaf9266c0ad2a1cb1e18730ae4?cid=null");
-              console.log("\n-------------\n");
+            if (data.tracks.items[0] === undefined) {
+                console.log("\n-------------\n");
+                console.log(" We could not fined what you searched for.\n")
+                console.log(" Song Title: The Sign\n Artist Name: Ace of Base\n Album Name: The Sign(US Album)\n Preview Link: https://p.scdn.co/mp3-preview/177e65fc2b8babeaf9266c0ad2a1cb1e18730ae4?cid=null");
+                console.log("\n-------------\n");
 
-            }else{
-              var item = data.tracks.items[0];
-              var artistName = item.artists[0].name;
-              var albumName = item.album.name;
-              var songTitle = item.name;
-              var previewLink = item.preview_url;
-              //It prints the Info of the Spefific track. Put in Quotes when user types on terminal.
-              console.log("\n-------------\n");
-              console.log("Song Title: " + songTitle + "\nArtist Name: " + artistName + "\nAlbum Name: " + albumName + "\nPreview Link: " + previewLink);
-              console.log("\n-------------\n");
+            } else {
+                var item = data.tracks.items[0];
+                var artistName = item.artists[0].name;
+                var albumName = item.album.name;
+                var songTitle = item.name;
+                var previewLink = item.preview_url;
+                //It prints the Info of the Spefific track. Put in Quotes when user types on terminal.
+                console.log("\n-------------\n");
+                console.log("Song Title: " + songTitle + "\nArtist Name: " + artistName + "\nAlbum Name: " + albumName + "\nPreview Link: " + previewLink);
+                console.log("\n-------------\n");
             }
 
             if (err) {
@@ -76,43 +76,32 @@ switch (action) {
         break;
 
     case "movie-this":
-    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
+        var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
 
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
+        // This line is just to help us debug against the actual URL.
+        console.log(queryUrl);
 
-    request(queryUrl, function(error, response, body) {
+        request(queryUrl, function(error, response, body) {
 
-      // If the request is successful
-      if (!error && response.statusCode === 200) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
 
-        if(title === undefined){
-          console.log("\n-------------\n");
-          console.log("You did not put any title! well, Here's Mr. Nobody \n")
-          console.log("Title: Mr. Nobody \nRelease Year: 2009\nIMDB Rating: 7.9  \nCountry: Belgium, Germany, Canada, France \nLanguage: English, Mohawk \nPlot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible. \nActors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham ");
-          console.log("\n-------------\n");
+                if (title === undefined) {
+                    console.log("\n-------------\n");
+                    console.log("You did not put any title! well, Here's Mr. Nobody \n")
+                    console.log("Title: Mr. Nobody \nRelease Year: 2009\nIMDB Rating: 7.9  \nCountry: Belgium, Germany, Canada, France \nLanguage: English, Mohawk \nPlot: A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision. As long as he doesn't choose, anything is possible. \nActors: Jared Leto, Sarah Polley, Diane Kruger, Linh Dan Pham ");
+                    console.log("\n-------------\n");
 
-        }else{
-        console.log("\n-------------\n");
-        console.log("Title: "+JSON.parse(body).Title+"\nRelease Year: "+JSON.parse(body).Year+ "\nIMDB Rating: "+JSON.parse(body).imdbRating+"\nCountry: "+JSON.parse(body).Country+"\nLanguage: "+JSON.parse(body).Language+"\nPlot: "+JSON.parse(body).Plot+"\nActors: "+JSON.parse(body).Actors);
-        console.log("\n-------------\n");
-        //Ned rotten Tomatoes rating and URL
-      }
+                } else {
+                    console.log("\n-------------\n");
+                    console.log("Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
+                    console.log("\n-------------\n");
+                    //Ned rotten Tomatoes rating and URL
+                }
 
-      }
-    });
+            }
+        });
 
-        // This will output the following information to your terminal/bash window:
-        // Title of the movie.
-        // * Year the movie came out.
-        // * IMDB Rating of the movie.
-        // * Country where the movie was produced.
-        // * Language of the movie.
-        // * Plot of the movie.
-        // * Actors in the movie.
-        // * Rotten Tomatoes Rating.
-        // * Rotten Tomatoes URL.
-        //http://www.omdbapi.com/?t=the+matrix&y=&plot=short&r=json
 
         break;
 
